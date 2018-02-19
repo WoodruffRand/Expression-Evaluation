6b46351eff5d7ea9013972c738a8ac0d4f356f5d
Randallwt <randallwt@gmail.com>
2018-02-19 02:16:19 -0500

    I did almost nohting, enjoy

--- src/app/Tester.java
+++ src/app/Tester.java
@@ -5,10 +5,21 @@ import java.util.ArrayList;
 public class Tester {
 	
 	public static void main(String [] args) {
+
+		testMakeVars();
+		
+		
+	}
+	
+	
+	public static void testMakeVars() {
 		ArrayList<Variable> vars = new ArrayList<>(); 
 		ArrayList<Array> arrays = new ArrayList<>();
-		
 		Expression.makeVariableLists("4*miners[10]-9/(y+20)", vars, arrays);
+		
+		assert(arrays.size()== 1);
+		assert(vars.size()== 1);
+		
 		
 	}
 
